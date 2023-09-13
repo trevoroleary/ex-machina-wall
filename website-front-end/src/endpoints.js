1a462dd2553cfe9b665a729e43579e60e2bcc587
Trevor O'Leary <git@trevoroleary.com>
2023-09-12 20:09:38 -0700

    using worker now

--- website-front-end/src/endpoints.js
+++ website-front-end/src/endpoints.js
@@ -1,19 +1,9 @@
 // math.js
 export function send_post(body, timeOfLastSend) {
     if (Date.now() - timeOfLastSend > 100){
-        fetch('https://ntfy.trevoroleary.com/x',{
+        fetch('https://expost.trevoroleary.com',{
         method: "POST",
         body: body,
-        headers: {
-          "Authorization": `Bearer tk_632ejha524dlfcgx7dnqnxb5in4sx`
-        }
-        })
-        // .then(response => response.json())
-        // .then(data => {
-        //   console.log(data); // Handle the response data here
-        // })
-        .catch(error => {
-            console.error('Error:', error);
         });
         return Date.now()
     }
