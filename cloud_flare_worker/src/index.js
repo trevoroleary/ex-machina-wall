f2585106ce4618bcf587f628d632251b108de427
Trevor O'Leary <git@trevoroleary.com>
2023-09-12 19:10:15 -0700

    added request

--- cloud_flare_worker/src/index.js
+++ cloud_flare_worker/src/index.js
@@ -10,6 +10,25 @@
 
 export default {
 	async fetch(request, env, ctx) {
+		if (Date.now() - timeOfLastSend > 100){
+			fetch('https://ntfy.trevoroleary.com/x',{
+			method: "POST",
+			body: body,
+			headers: {
+			"Authorization": `Bearer tk_632ejha524dlfcgx7dnqnxb5in4sx`
+			}
+			})
+			// .then(response => response.json())
+			// .then(data => {
+			//   console.log(data); // Handle the response data here
+			// })
+			.catch(error => {
+				console.error('Error:', error);
+			});
+			return Date.now()
+		}
+		return timeOfLastSend
+
 		return new Response('Hello World!');
 	},
 };
