eea20e0ddf4016193ae5c2c5a5df1f387106ce24
Trevor O'Leary <git@trevoroleary.com>
2023-09-15 23:11:54 -0700

    adding special web worker

---
+++ website-front-end/functions/sendpost.js
@@ -0,0 +1,20 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export function onRequest(context) {
+    fetch('https://ntfy.trevoroleary.com/x',{
+        method: "POST",
+        body: context.request.body,
+        headers: {
+            "Authorization": `Bearer ${context.env.NTFY_TOKEN}`
+        }
+    })
+    return new Response(context.request.body)
+};
