7f41c42fbdcb3b9d47d9370e816085f08cb35a0d
Trevor O'Leary <git@trevoroleary.com>
2023-09-15 23:25:14 -0700

    remove all keys and log work

--- website-front-end/src/endpoints.js
+++ website-front-end/src/endpoints.js
@@ -1,22 +1,11 @@
 // math.js
 export function send_post(body, timeOfLastSend) {
-    if (Date.now() - timeOfLastSend > 100){
-        fetch('https://ex.trevoroleary.com/sendpost',{
-          method: "POST",
-          body: body,
-        })
-        .then(response => response.json())
-        .then(data => {
-          console.log(data); // Handle the response data here
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-        return Date.now()
-    }
-    return timeOfLastSend
+  if (Date.now() - timeOfLastSend > 100){
+      fetch('https://ex.trevoroleary.com/sendpost',{
+        method: "POST",
+        body: body,
+      })
+      return Date.now()
   }
-  
-  export function subtract(a, b) {
-    return a - b;
-  }
+  return timeOfLastSend
+}
