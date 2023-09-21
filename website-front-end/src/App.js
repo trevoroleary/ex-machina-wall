ccde3178b856e217dfcd635487b907a58f28c2b2
Trevor O'Leary <git@trevoroleary.com>
2023-09-20 20:19:20 -0700

    Done?

--- website-front-end/src/App.js
+++ website-front-end/src/App.js
@@ -3,6 +3,8 @@ import React, { useState, Fragment }  from 'react';
 import Slider from '@mui/material/Slider'
 import { TextField } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
+import { Button } from '@mui/material';
+
 
 import darkTheme from './darkTheme';
 import lightTheme from './lightTheme';
@@ -22,30 +24,18 @@ function App() {
     setPrimaryHSVA({ ...primaryHSVA, ...color.hsva })
     setTimeOfLastSend(send_post(`SET_PRIMARY_COLOR-${color.rgb.r}-${color.rgb.g}-${color.rgb.b}`, timeOfLastSend))
   }
-
-  const handlePrimaryBrightnessChange = (event) => {
-    setTimeOfLastSend(send_post(`SET_PRIMARY_BRIGHTNESS-${event.target.value}`, timeOfLastSend))
-  }
   
   const handleSecondaryColorChange = (color) => {
     setSecondaryHSVA({ ...secondaryHSVA, ...color.hsva })
     setTimeOfLastSend(send_post(`SET_SECONDARY_COLOR-${color.rgb.r}-${color.rgb.g}-${color.rgb.b}`, timeOfLastSend))
   }
 
-  const handleSecondaryBrightnessChange = (event) => {
-    setTimeOfLastSend(send_post(`SET_SECONDARY_BRIGHTNESS-${event.target.value}`, timeOfLastSend))
+  const handleButtonPressed = (event) => {
+    setTimeOfLastSend(send_post(event.target.id, timeOfLastSend))
   }
 
-  const handleImageBrightnessChange = (event) => {
-    setTimeOfLastSend(send_post(`SET_IMAGE_BRIGHTNESS-${event.target.value}`, timeOfLastSend))
-  }
-
-  const handleArduinoPGainChanged = (event) => {
-    setTimeOfLastSend(send_post(`SET_ARDUINO_PGAIN-${event.target.value}`, timeOfLastSend))
-  }
-
-  const handleFrameTimeChanged = (event) => {
-    setTimeOfLastSend(send_post(`SET_FRAME_TIME-${event.target.value}`, timeOfLastSend))
+  const handleSliderChange = (event) => {
+    setTimeOfLastSend(send_post(`${event.target.name}-${event.target.value}`, timeOfLastSend))
   }
 
   const handleImageLinkChanged = (event) => {
@@ -64,62 +54,105 @@ function App() {
           </p>
         </div>
         
-        <div>
-        <p className='pb-3'>Primary Color</p>
-          <div className="flex flex-row">
+        <div className='max-w-sm w-full'>
+          <p className='pb-3'>Primary Color</p>
+          <div className="flex flex-row mx-auto w-min">
             <div className="flex-1">
             <Fragment>
               <Wheel color={primaryHSVA} onChange={handlePrimaryColorChange}/>
             </Fragment>
             </div>
-            <div className="flex-1">
+            <div>
             <Slider
               defaultValue={100} step={1} min={0} max={100} valueLabelDisplay='auto' orientation="vertical"
-              onChange={handlePrimaryBrightnessChange}
+              name="SET_PRIMARY_BRIGHTNESS"
+              onChange={handleSliderChange}
             ></Slider>
             </div>
           </div>
         </div>
         
-        <div>
-        <p className='pb-3'>Secondary Color</p>
-          <div className="flex flex-row">
+        <div className='max-w-sm w-full'>
+          <p className='pb-3 pt-4'>Secondary Color</p>
+          <div className="flex flex-row w-min mx-auto">
             <div className="flex-1">
               <Fragment>
                 <Wheel color={secondaryHSVA} onChange={handleSecondaryColorChange}/>
               </Fragment>
             </div>
-            <div className="flex-1">
+            <div>
             <Slider 
               defaultValue={100} step={1} min={0} max={100} valueLabelDisplay='auto' orientation="vertical"
-              onChange={handleSecondaryBrightnessChange}
+              name="SET_SECONDARY_BRIGHTNESS"
+              onChange={handleSliderChange}
             ></Slider>
             </div>
           </div>
         </div>
 
-        <div>
-          <p className='pb-3'>Image/Animation</p>
+        <div className='max-w-sm w-full'>
+          <p className='pb-1 pt-4'>Audio React Settings</p>
+          <div className='flex flex-row w-full mx-auto'>
+            <div className="flex flex-col">
+              <div className='flex flex-row'>
+                <Button onClick={handleButtonPressed} id="SET_HIGH_FREQUENCY_REACT_STATE-ON">High_Freq_On</Button>
+                <Button onClick={handleButtonPressed} id="SET_HIGH_FREQUENCY_REACT_STATE-OFF">High_Freq_Off</Button>
+              </div>
+              
+              <div className='flex flex-col'>
+                <p className='pr-5'>Threshold</p>
+                <Slider 
+                defaultValue={170000} step={1000} min={10000} max={250000} valueLabelDisplay='auto'
+                name="SET_HIGH_FREQUENCY_THRESHOLD"
+                onChange={handleSliderChange}
+                ></Slider>
+              </div>
+
+              <div className='flex flex-row flex-1'>
+                <Button onClick={handleButtonPressed} id="SET_LOW_FREQUENCY_REACT_STATE-ON">LOW_Freq_On</Button>
+                <Button onClick={handleButtonPressed} id="SET_LOW_FREQUENCY_REACT_STATE-OFF">LOW_Freq_Off</Button>
+              </div>
+
+              <div className='flex flex-col'>
+                <p>Threshold</p>
+                <Slider 
+                defaultValue={300000} step={1000} min={30000} max={500000} valueLabelDisplay='auto'
+                name="SET_LOW_FREQUENCY_THRESHOLD"
+                onChange={handleSliderChange}
+                ></Slider>
+              </div>
+
+            </div>
+          </div>
+          
+        </div>
+
+        <div className='max-w-sm w-full'>
+          <p className='pb-3 pt-4'>Image/Animation</p>
           <div className="flex flex-row">
             <div className="flex-1">
               <TextField 
                 id="outlined-basic" label="GIF/Image Link" variant="outlined" onKeyDown={handleImageLinkChanged}
+                fullWidth
               />
               <p className='pt-3'>GIF Frame Time</p>
               <Slider
                 defaultValue={0.1} step={0.01} min={0.01} max={0.5} valueLabelDisplay='auto'
-                onChange={handleFrameTimeChanged}
+                name="SET_FRAME_TIME"
+                onChange={handleSliderChange}
               ></Slider>
               <p>Arduino P Gain</p>
               <Slider
                 defaultValue={20} step={1} min={1} max={100} valueLabelDisplay='auto'
-                onChange={handleArduinoPGainChanged}
+                name="SET_ARDUINO_PGAIN"
+                onChange={handleSliderChange}
               ></Slider>
             </div>
             <div>
               <Slider 
                 defaultValue={100} step={1} min={0} max={100} valueLabelDisplay='auto' orientation="vertical"
-                onChange={handleImageBrightnessChange}
+                id="SET_IMAGE_BRIGHTNESS"
+                onChange={handleSliderChange}
               ></Slider>
             </div>
           </div>
--- website-front-end/src/endpoints.js
+++ website-front-end/src/endpoints.js
@@ -1,9 +1,16 @@
 // math.js
 export function send_post(body, timeOfLastSend) {
   if (Date.now() - timeOfLastSend > 100){
-      fetch('https://ex.trevoroleary.com/sendpost',{
+      // fetch('https://ex.trevoroleary.com/sendpost',{
+      //   method: "POST",
+      //   body: body,
+      // })
+      fetch('https://ntfy.trevoroleary.com/x',{
         method: "POST",
         body: body,
+        headers: {
+            "Authorization": `Bearer tk_632ejha524dlfcgx7dnqnxb5in4sx`
+        }
       })
       return Date.now()
   }
